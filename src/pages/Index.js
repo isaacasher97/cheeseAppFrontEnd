9912c2b6470892f01a18d904562ddc5c0c7c77ea
isaacasher97 <isaac.asher97@gmail.com>
2023-08-13 04:44:20 -0400

    added form

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,17 +1,30 @@
-import { Link, useLoaderData } from "react-router-dom"
+import { Form, Link, useLoaderData } from "react-router-dom"
 
 function Index(props) {
   const cheeses = useLoaderData()
 
-  return cheeses.map(cheese => (
-    <div key={cheese._id} className="person">
-      <Link to={`/${cheese._id}`}>
-        <h1>{cheese.name}</h1>
-      </Link>
-      <img src={cheese.image} alt={cheese.name} />
-      <h3>{cheese.countryOfOrigin}</h3>
+  return (
+    <div>
+      <h2>Create a Cheese</h2>
+      <Form action="/create" method="post">
+        <input type="input" name="name" placeholder="Cheese Name" />
+        <input type="input" name="image" placeholder="Image URL" />
+        <input type="input" name="countryOfOrigin" placeholder="Country Of Origin" />
+        <input type="submit" value="Create Cheese" />
+      </Form>
+
+      <h2>Cheese</h2>
+      {cheeses.map(cheese => (
+        <div key={cheese._id} className="person">
+          <Link to={`/${cheese._id}`}>
+            <h1>{cheese.name}</h1>
+          </Link>
+          <img src={cheese.image} alt={cheese.name} />
+          <h3>{cheese.title}</h3>
+        </div>
+      ))}
     </div>
-  ))
+  )
 }
 
 export default Index
